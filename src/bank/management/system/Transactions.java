6d3b0029af52c0848af5ef23d7959e373148c205
Yashthakare65 <yash44thakare@gmail.com>
2026-06-27 21:02:40 +0530

    Various Buttons are added in transaction class on atm image

--- src/bank/management/system/Transactions.java
+++ src/bank/management/system/Transactions.java
@@ -3,9 +3,12 @@ package bank.management.system;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.*;
 
 
-public class Transactions extends JFrame {
+public class Transactions extends JFrame implements ActionListener{
+    
+    JButton deposit,withdrawl,ministatement,pinchange,fastcash,balanceenquiry,exit;
     
     Transactions(){
         
@@ -23,12 +26,52 @@ public class Transactions extends JFrame {
         text.setFont(new Font("System",Font.BOLD,16));
         image.add(text);
         
+        deposit = new JButton("Deposit");
+        deposit.setBounds(160,315,150,30);
+        deposit.addActionListener(this);
+        image.add(deposit);
+        
+        withdrawl = new JButton("Cash Withdrawl");
+        withdrawl.setBounds(350,315,150,30);
+        withdrawl.addActionListener(this);
+        image.add(withdrawl);
+        
+        fastcash = new JButton("Fast Cash");
+        fastcash.setBounds(160,350,150,30);
+        fastcash.addActionListener(this);
+        image.add(fastcash);
+        
+        ministatement = new JButton("Mini Statement");
+        ministatement.setBounds(350,350,150,30);
+        ministatement.addActionListener(this);
+        image.add(ministatement);
+        
+        pinchange = new JButton("Pin Change");
+        pinchange.setBounds(160,385,150,30);
+        pinchange.addActionListener(this);
+        image.add(pinchange);
+        
+        balanceenquiry = new JButton("Balance Enquiry");
+        balanceenquiry.setBounds(350,385,150,30);
+        balanceenquiry.addActionListener(this);
+        image.add(balanceenquiry);
+        
+        exit = new JButton("Exit");
+        exit.setBounds(350,420,150,30);
+        exit.addActionListener(this);
+        image.add(exit);
         
         setSize(900,900);
         setLocation(300,0);
         //setUndecorated(true);
         setVisible(true);
         
+    }
+    
+    public void actionPerformed(ActionEvent ae){
+        if (ae.getSource()== exit){
+            System.exit(0);
+        }
     }
     
     public static void main(String args[]){
